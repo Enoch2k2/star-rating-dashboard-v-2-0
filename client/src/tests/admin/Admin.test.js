c17c38d497b50cdcf5cef337124bde2f557c5568
Enoch2k2 <enoch2k2@hotmail.com>
2019-07-31 09:55:23 -0500

    added tests for navbar

--- client/src/tests/admin/Admin.test.js
+++ client/src/tests/admin/Admin.test.js
@@ -4,9 +4,25 @@ import '../setupTests';
 import { shallow } from 'enzyme';
 import App from '../../App';
 import { AdminLayout } from '../../containers/admin';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { AdminNavbar } from '../../components/admin';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-it('Root route renders Admin Layout if logged in as admin', () => {
-  const wrapper = shallow(<App />);
-  expect(wrapper.contains(<Route exact path="/" component={ AdminLayout } />)).toEqual(true);
-});
+describe('Admin', () => {
+  it('Root route renders Admin Layout if logged in as admin', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.contains(<Route exact path="/" component={ AdminLayout } />)).toEqual(true);
+  });
+  
+  it('AdminLayout container includes the AdminNavbar', () => {
+    const wrapper = shallow(<AdminLayout />);
+    expect(wrapper.contains(<AdminNavbar />)).toEqual(true);
+  });
+  
+  it('AdminNavbar component includes all admin links', () => {
+    const wrapper = shallow(<AdminNavbar />);
+    expect(wrapper.find(Link).first().props().children).toEqual("Home");
+    expect(wrapper.find(Link).at(1).props().children).toEqual("Roles");
+    expect(wrapper.find(Link).at(2).props().children).toEqual("Technical Coach List");
+    expect(wrapper.find(Link).at(3).props().children).toEqual("Overall Ratings");
+  });
+})
