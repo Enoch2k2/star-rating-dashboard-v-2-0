e5b70521dc24e07bcde19fe8eb4803ddd637f9f4
Enoch2k2 <enoch2k2@hotmail.com>
2019-08-09 15:33:19 -0500

    added test for user attributes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_11_195035) do
+ActiveRecord::Schema.define(version: 2019_08_09_201649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_07_11_195035) do
     t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
